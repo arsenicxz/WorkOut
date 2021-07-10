2d06c2fa99b9ea7bfc9895d0b2007e9e9f837cc1
NANI-cant <trixxx.asd@yandex.ru>
2021-07-11 01:39:52 +0300

    base server ready

---
+++ Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            System.Console.WriteLine("Registration\n");
+            System.Console.WriteLine("Login");
+            string login = System.Console.ReadLine();
+            System.Console.WriteLine("Password");
+            string password = System.Console.ReadLine();
+
+            Dictionary<string, string> parametrs = new Dictionary<string, string>()
+            {
+                { "Login", login },
+                { "Password", password },
+
+            };
+            var content = new FormUrlEncodedContent(parametrs);
+
+            //var request = httpClient.GetAsync("http://localhost:8714/",content).Result;
+            var request2 = httpClient.PostAsync("http://localhost:8714/", content).Result;
+            var requestContent = request2.Content.ReadAsStringAsync().Result;
+            File.WriteAllText("index.html", requestContent);
+        }
+    }
+}
