95e092d9d2f4e29d30994c70b2f44fea769170ac
NANI-cant <trixxx.asd@yandex.ru>
2021-07-10 14:51:59 +0300

    sss

--- Server/RemoteServer.cs
+++ Server/RemoteServer.cs
@@ -15,7 +15,7 @@ namespace ExampleTcpListener_Console
                 // Определим нужное максимальное количество потоков
                 // Пусть будет по 4 на каждый процессор
                 int MaxThreadsCount = Environment.ProcessorCount * 4;
-                Console.WriteLine(MaxThreadsCount.ToString());
+                Console.WriteLine("MaxThreadsCount = " + MaxThreadsCount.ToString());
                 // Установим максимальное количество рабочих потоков
                 ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
                 // Установим минимальное количество рабочих потоков
@@ -24,7 +24,7 @@ namespace ExampleTcpListener_Console
 
                 // Устанавливаем порт для TcpListener = 9595.
                 Int32 port = 9595;
-                System.Net.IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
                 int counter = 0;
                 server = new TcpListener(IPAddress.Any, port);
 
@@ -97,11 +97,8 @@ namespace ExampleTcpListener_Console
                 stream.Write(msg, 0, msg.Length);
 
             }
-
             // Закрываем соединение.
             client.Close();
-
-
         }
     }
 }
